0471e97b3082a213280fc7bb9feb311adcdfade2
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-06 06:34:29 -0400

    Create Table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -4,8 +4,7 @@
 --- email, string (255 characters), never null and unique
 --- name, string (255 characters)
 CREATE TABLE IF NOT EXISTS users (
-    id int NOT NULL AUTO_INCREMENT,
-    email nvarchar(255) NOT NULL UNIQUE,
-    name nvarchar(255),
-    PRIMARY KEY(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
