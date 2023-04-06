f84918ae03df9a8a05a468d013e50c71886635e8
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-06 06:55:31 -0400

    Create Table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,5 @@
--- SQL script that creates a table users.
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
+-- SQL script/statements that creates a table users.
+CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT,
     email nvarchar(255) NOT NULL UNIQUE,
     name nvarchar(255),
