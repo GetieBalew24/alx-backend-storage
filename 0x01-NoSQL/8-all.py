60a37289ddc96422b16e8d1d1d985616d71e9b13
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-11 13:02:51 -0400

    delete all document

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ a script that deletes all documents with name="Holberton school 
-The database name passed as option of mongo command
+The database name passed as option of mongo command.
 """
 
 
 def list_all(mongo_collection):
-    """ Lists all documents in a collection."""
+    """ Lists all documents in a school collection."""
     return [doc for doc in mongo_collection.find()]
